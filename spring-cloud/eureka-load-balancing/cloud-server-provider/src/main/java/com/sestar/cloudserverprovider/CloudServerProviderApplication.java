4434de6d543a31bd0cc8a72a098d7ba01b8177d2
Sestar <ScorpionSeStar@outlook.com>
2019-01-10 14:13:42 +0800

    add eureka-load-balancing project

---
+++ spring-cloud/eureka-load-balancing/cloud-server-provider/src/main/java/com/sestar/cloudserverprovider/CloudServerProviderApplication.java
@@ -0,0 +1,14 @@
+package com.sestar.cloudserverprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudServerProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudServerProviderApplication.class, args);
+    }
+
+}
+
